8f03cb8b896f9fa04796d779debfb3ece0772a4c
wcourtney31415 <wcourtney31415@gmail.com>
2020-10-12 22:06:42 -0400

    Implemented the builder pattern on the User class.

--- src/main/java/com/wesleycourtney/App.java
+++ src/main/java/com/wesleycourtney/App.java
@@ -5,45 +5,30 @@ import static spark.Spark.*;
 import org.apache.log4j.BasicConfigurator;
 
 import com.google.gson.Gson;
+import com.wesleycourtney.UserFactory.User;
 
 import spark.Spark;
 
 public class App {
-	
+
 	public static void main(String[] args) {
 		BasicConfigurator.configure();
 		Spark.port(80);
-		
+
 		Gson gson = new Gson();
-		User rhonda = new User();
-		rhonda.firstName = "Rhonda";
-		rhonda.lastName = "Holloway";
-		rhonda.phoneNumber = "(526) 141-3395";
-		rhonda.email = "elit.erat.vitae@vitaenibhDonec.net";
-		rhonda.birthdate = "01-24-10";
-		rhonda.streetAddress = "Ap #509-9596 Auctor, Rd.";
-		rhonda.city = "Chepstow";
-		rhonda.zip = "6152";
-		rhonda.country = "Malta";
-		
+		User rhonda = new UserFactory()
+				.setFirstName("Rhonda")
+				.setLastName("Holloway")
+				.setPhoneNumber("(526) 141-3395")
+				.setEmail("elit.erat.vitae@vitaenibhDonec.net")
+				.setBirthdate("01-24-10")
+				.setStreetAddress("Ap #509-9596 Auctor, Rd.")
+				.setCity("Chepstow")
+				.setZip("6152")
+				.setCountry("Malta")
+				.build();
+
 		get("/hello", (req, res) -> gson.toJson(rhonda));
-	}
-
-}
-
-class User {
-	String firstName;
-	String lastName;
-	String phoneNumber;
-	String email;
-	String birthdate;
-	String streetAddress;
-	String city;
-	String zip;
-	String country;
-
-	User() {
-
 	}
 
 }
--- src/main/java/com/wesleycourtney/UserFactory.java
+++ src/main/java/com/wesleycourtney/UserFactory.java
@@ -1,5 +1,127 @@
 package com.wesleycourtney;
 
 public class UserFactory {
-	
+
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String email;
+	private String birthdate;
+	private String streetAddress;
+	private String city;
+	private String zip;
+	private String country;
+
+	public UserFactory setFirstName(String firstName) {
+		this.firstName = firstName;
+		return this;
+	}
+
+	public UserFactory setLastName(String lastName) {
+		this.lastName = lastName;
+		return this;
+	}
+
+	public UserFactory setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+		return this;
+	}
+
+	public UserFactory setEmail(String email) {
+		this.email = email;
+		return this;
+	}
+
+	public UserFactory setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+		return this;
+	}
+
+	public UserFactory setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+		return this;
+	}
+
+	public UserFactory setCity(String city) {
+		this.city = city;
+		return this;
+	}
+
+	public UserFactory setZip(String zip) {
+		this.zip = zip;
+		return this;
+	}
+
+	public UserFactory setCountry(String country) {
+		this.country = country;
+		return this;
+	}
+
+	public User build() {
+		User newUser = new User();
+		newUser.setFirstName(firstName);
+		newUser.setLastName(lastName);
+		newUser.setPhoneNumber(phoneNumber);
+		newUser.setEmail(email);
+		newUser.setBirthdate(birthdate);
+		newUser.setStreetAddress(streetAddress);
+		newUser.setCity(city);
+		newUser.setZip(zip);
+		newUser.setCountry(country);
+		return newUser;
+	}
+
+	public class User {
+
+		private String firstName;
+		private String lastName;
+		private String phoneNumber;
+		private String email;
+		private String birthdate;
+		String streetAddress;
+		String city;
+		String zip;
+		String country;
+
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+		public void setPhoneNumber(String phoneNumber) {
+			this.phoneNumber = phoneNumber;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public void setBirthdate(String birthdate) {
+			this.birthdate = birthdate;
+		}
+
+		public void setStreetAddress(String streetAddress) {
+			this.streetAddress = streetAddress;
+		}
+
+		public void setCity(String city) {
+			this.city = city;
+		}
+
+		public void setZip(String zip) {
+			this.zip = zip;
+		}
+
+		public void setCountry(String country) {
+			this.country = country;
+		}
+
+		private User() {
+
+		}
+
+	}
 }
